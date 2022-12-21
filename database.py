30ef72d4c9f463e472f2da3514fb0c7c465cb978
Anthony Flori <aflori@wearehunty.fr>
2022-12-21 22:00:58 +0100

    tt

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ import sqlalchemy.ext.declarative as _declarative
 import sqlalchemy.orm as _orm
 
 
-DATABASE_URL = "postgresql://cgsukvjjfzlgrd:e980af14f1b9a825d213103d365bb33daeee148528d94274a2f3c8181ca81bec@ec2-52-30-67-143.eu-west-1.compute.amazonaws.com:5432/demsi71c8nc1no"
+DATABASE_URL = "postgres://cgsukvjjfzlgrd:e980af14f1b9a825d213103d365bb33daeee148528d94274a2f3c8181ca81bec@ec2-52-30-67-143.eu-west-1.compute.amazonaws.com:5432/demsi71c8nc1no"
 
 
 engine = _sql.create_engine(DATABASE_URL)
